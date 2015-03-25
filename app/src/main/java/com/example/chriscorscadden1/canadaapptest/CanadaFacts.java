85ce45e5c9abe39b421c425c3afdc078eef2031d
Drewx118 <ccorscadden118@gmail.com>
2015-03-25 20:04:59 +1100

    Added CanadaFacts class
    Added CanadaFacts class which stores title and the json array rows

---
+++ app/src/main/java/com/example/chriscorscadden1/canadaapptest/CanadaFacts.java
@@ -0,0 +1,14 @@
+package com.example.chriscorscadden1.canadaapptest;
+
+/**
+ * Created by chriscorscadden1 on 25/03/2015.
+ */
+public class CanadaFacts {
+
+    public String title;
+    public String[][] rows;
+
+    public CanadaFacts(){
+
+    }
+}
