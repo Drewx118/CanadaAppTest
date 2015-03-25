2ca951d698be9b1808c4de79f05839ca1a0ddcdb
Drewx118 <ccorscadden118@gmail.com>
2015-03-25 19:44:01 +1100

    Created Fact class
    Created fact class to store the json data for each fact

---
+++ app/src/main/java/com/example/chriscorscadden1/canadaapptest/Fact.java
@@ -0,0 +1,16 @@
+package com.example.chriscorscadden1.canadaapptest;
+
+/**
+ * Created by chriscorscadden1 on 25/03/2015.
+ */
+
+public class Fact {
+
+    public String title;
+    public String description;
+    public String imageHref;
+
+    public Fact(){
+
+    }
+}
